1f96823d4b85547d201930d0dae61e6418765a2e
Mustafa KOPARIR <mkoparir@gmail.com>
2021-10-13 22:18:19 +0300

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { KopMatModule } from './material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sayfa/yerlesim/sidebar/sidebar.component';
+import { HeaderComponent } from './sayfa/yerlesim/header/header.component';
+import { FooterComponent } from './sayfa/yerlesim/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    KopMatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
